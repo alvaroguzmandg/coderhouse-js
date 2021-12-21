cd6d28995015b6d9202b28767ee0f4c711a5a43b
alvaroguzmandg <aguzman@VERSARDMC003.local>
2021-12-21 18:55:24 -0300

    Ajustes 3er entrega

---
+++ PRUEBAFILTRO/creacionCarreras.js
@@ -0,0 +1,40 @@
+let carrera = [];
+
+class Carrera {
+    constructor(id, nombre, distancia, fecha, lugar, horario, foto, estado) {
+        this.id = id;
+        this.nombre = nombre;
+        this.distancia = distancia;
+        this.fecha = fecha;
+        this.lugar = lugar;
+        this.horario = horario;
+        this.foto = foto;
+        this.estado = estado;
+    }
+}
+
+$.getJSON("https://alvaroguzmandg.github.io/coderhouse-js/Clase14-Ajax_jQuery/carreras.json", function(data) {
+    data.forEach(elemento => carrera.push(elemento))
+        //Función para cargar las carreras en el DOM
+    calendarioDeCarreras();
+})
+
+function calendarioDeCarreras() {
+    carrera.forEach((carrera) => {
+
+        $("#contenedorEventos").append(`
+        <div class="contenedorCarrera" id="contenedorCarrera${carrera.id}">
+            <h2 id="tituloCarrera" class="tituloCarrera">${carrera.nombre}</h2>
+            <img src="${carrera.foto}" class="fotoCarrera">
+            <span class="fechaHora">
+                <span class="fechaCarrera">${carrera.fecha}</span>
+                <span class="horarioCarrera">Horario: ${carrera.horario}</span>
+            </span>
+            <span class="lugarCarrera">${carrera.lugar}</span>
+            <button id="btnInscripcion${carrera.id}" class="btRunDestacado-large">¡INSCRIBIRME!</button>
+        </div>
+
+        `)
+
+    })
+}
---
+++ PRUEBAFILTRO/filtros.js
@@ -0,0 +1,10 @@
+$("#filtros").append(`
+        <div class="contenedorFiltros id="contenedorFiltros">
+        <button id="carreras5k" class="btRun-sm">5K</button>
+        <button id="carreras10k" class="btRun-sm">10K</button>
+        <button id="carreras21k" class="btRun-sm">21K</button>
+        <button id="carreras42k" class="btRun-sm">42K</button>
+        <button id="carreras100k" class="btRun-sm">100K</button>
+        </div>
+
+        `)
